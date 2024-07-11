2f40ad674aee7ae5741c5c3087c35a0e0470573c
Johannes Reppin <johannes.reppin@desy.de>
2024-07-11 22:41:00 +0200

    use temurin 21 build image

--- src/main/java/com/github/thomasdarimont/keycloak/healthchecker/support/KeycloakUtil.java
+++ src/main/java/com/github/thomasdarimont/keycloak/healthchecker/support/KeycloakUtil.java
@@ -1,10 +1,14 @@
 package com.github.thomasdarimont.keycloak.healthchecker.support;
 
 import org.keycloak.utils.KeycloakSessionUtil;
+import org.jboss.logging.Logger;
 
 public class KeycloakUtil {
 
+    private final static Logger logger = Logger.getLogger(KeycloakUtil.class);
+
     public static boolean isRunningOnKeycloak() {
+        logger.info(Holder.PROVIDER_NAME);
         return Holder.RUNNING_ON_KEYCLOAK;
     }
 
@@ -15,14 +19,13 @@ public class KeycloakUtil {
     private static class Holder {
 
         private static final boolean RUNNING_ON_KEYCLOAK;
+        private static final String PROVIDER_NAME;
 
         static {
 
-            final String PROVIDER_NAME;
-            RUNNING_ON_KEYCLOAK = KeycloakSessionUtil.getKeycloakSession().getClass().getSimpleName()
-                    .equals("Resteasy3Provider");
             PROVIDER_NAME = KeycloakSessionUtil.getKeycloakSession().getClass().getSimpleName();
-            System.out.println(PROVIDER_NAME);
+            RUNNING_ON_KEYCLOAK = PROVIDER_NAME.equals("Resteasy3Provider");
         }
+
     }
 }
